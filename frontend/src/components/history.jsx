641322419526f6f390bc83b6b02c747805122455
Colby <colbybarrett99@gmail.com>
2024-03-26 19:28:14 -0600

    Undo table

--- frontend/src/components/history.jsx
+++ frontend/src/components/history.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
-//import "./popUp.css";
-
+import "./popUp.css";
 
 const History = (props) => (
     <tr>
@@ -27,14 +26,14 @@ const ModalTwo = ({open, onClose}) => {
         let history = data.history;
         setSelectedAccount(history);
         document.getElementById('history').innerHTML = history.map(record => 
-            `
-              <td>${record.account}</td>
-              <td>Transaction Date: ${record.date}</td>
-              <td>Transaction Type: ${record.type}</td>
-              <td>Transaction Amount: ${record.amount}</td> 
-              <td>Transaction Description: ${record.description}</td>
+            `<div>
+              <div>Account Name: ${record.account}</div>
+              <div>Transaction Date: ${record.date}</div>
+              <div>Transaction Type: ${record.type}</div>
+              <div>Transaction Amount: ${record.amount}</div> 
+              <div>Transaction Description: ${record.description}</div>
               <br><br>
-            </table>`
+            </div>`
         ).join('');
     };
 
@@ -49,13 +48,14 @@ const ModalTwo = ({open, onClose}) => {
         let history = data.history;
         setSelectedAccount(history);
         document.getElementById('history').innerHTML = history.map(record => 
-            `
-              <td>Account Name: ${record.account}</td>
-              <td>Transaction Date: ${record.date}</td>
-              <td>Transaction Type: ${record.type}</td>
-              <td>Transaction Amount: ${record.amount}</td> 
-              <td>Transaction Description: ${record.description}</td>
+            `<div>
+              <div>Account Name: ${record.account}</div>
+              <div>Transaction Date: ${record.date}</div>
+              <div>Transaction Type: ${record.type}</div>
+              <div>Transaction Amount: ${record.amount}</div> 
+              <div>Transaction Description: ${record.description}</div>
               <br><br>
+            </div>`
         ).join('');
         };
 
@@ -66,30 +66,15 @@ const ModalTwo = ({open, onClose}) => {
                     <p onClick={onClose} className="txt">X</p>
                     <br></br>
                     <h1>History</h1>
-                    <div classname="input">
-                        <input type="radio" id="checking" name="selected_account" value="checking" onChange={handleAccountChange} />
-                        <label htmlFor="checking">Checking</label>
-                        <input type="radio" id="savings" name="selected_account" value="savings" onChange={handleAccountChange} />
-                        <label htmlFor="savings">Savings</label>
-                        <input type="radio" id="yield" name="selected_account" value="yield" onChange={handleAccountChange} />
-                        <label htmlFor="yield">High Yield</label>
-                    </div>
-                    <br></br>
-                    
-                    <div classname="input">
-                        <button onClick={handleShowHistory}>Show History for Specific Account</button>
-                        <button onClick={handleShowEntireHistory}>Show History for Entire Account</button>
-                        <table>
-                            <th>Name</th>
-                            <th>Transaction Date</th>
-                            <th>Transaction Type</th>
-                            <th>Transaction Amount</th>
-                            <th>Transaction Description</th>
-                            <tr id="history">
-
-                            </tr>
-                        </table>
-                    </div>
+                    <input type="radio" id="checking" name="selected_account" value="checking" onChange={handleAccountChange} />
+                    <label htmlFor="checking">Checking</label>
+                    <input type="radio" id="savings" name="selected_account" value="savings" onChange={handleAccountChange} />
+                    <label htmlFor="savings">Savings</label>
+                    <input type="radio" id="yield" name="selected_account" value="yield" onChange={handleAccountChange} />
+                    <label htmlFor="yield">High Yield</label>
+                    <button onClick={handleShowHistory}>Show History for Specific Account</button>
+                    <button onClick={handleShowEntireHistory}>Show History for Entire Account</button>
+                    <h3 id="history"></h3>
                 </div>
             </div>
     );
